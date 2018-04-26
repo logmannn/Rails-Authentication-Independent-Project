b3505546d29fd13f5684c74911cf8779dd9909b4
Guest User <Guest@Epicodus-5C.local>
2018-04-26 15:07:32 -0700

    comments shows user_id number

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :current_password, :first_name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :username, :email, :password])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :username, :first_name, :current_password, :first_name, :username])
   end
 
 end
